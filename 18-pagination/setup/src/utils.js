4863aaecbbcd19045375833300d5c64c795f3423
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-10-21 14:52:36 +0100

    Pagination logic

--- 18-pagination/setup/src/utils.js
+++ 18-pagination/setup/src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 11;
+  const pages = Math.ceil(followers.length / itemsPerPage);
+  console.log(pages);
 
-export default paginate
+  const newFollowers = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemsPerPage;
+    return followers.slice(start, start + itemsPerPage);
+  });
+  return newFollowers;
+};
+
+export default paginate;
