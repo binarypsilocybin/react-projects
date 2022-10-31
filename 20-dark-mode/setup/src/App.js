7a31aef5cefb97145932d023a246f1bce2330a76
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-10-31 12:30:19 +0000

    localStorage

--- 20-dark-mode/setup/src/App.js
+++ 20-dark-mode/setup/src/App.js
@@ -2,13 +2,38 @@ import React, { useState, useEffect, isValidElement } from "react";
 import data from "./data";
 import Article from "./Article";
 
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
 function App() {
+  const [theme, setTheme] = useState(getStorageTheme);
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
   return (
     <main>
       <nav>
         <div className="nav-center">
           <h1>dark mode starter</h1>
-          <button className="btn">toggle</button>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
         </div>
       </nav>
       <section className="articles">
