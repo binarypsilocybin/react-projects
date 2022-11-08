71f07869e0201a53403687c62c28d31dc2eaccdc
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-11-08 23:45:23 +0000

    setup form control

--- 23-quiz/setup/src/SetupForm.js
+++ 23-quiz/setup/src/SetupForm.js
@@ -1,8 +1,40 @@
-import React from 'react'
-import { useGlobalContext } from './context'
+import React from "react";
+import { useGlobalContext } from "./context";
 
 const SetupForm = () => {
-  return <h2>setup form</h2>
-}
+  const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
 
-export default SetupForm
+  return (
+    <main>
+      <section className="quiz quiz-small">
+        <form className="setup-form">
+          <h2>setup quiz</h2>
+          {/*amount */}
+          <div className="form-control">
+            <label htmlFor="amount">number of questions</label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              value={quiz.amount}
+              onChange={handleChange}
+              className="form-input"
+              min={1}
+              max={50}
+            />
+          </div>
+          {error && (
+            <p className="error">
+              can't generate questions, please try different options
+            </p>
+          )}
+          <button type="submit" onClick={handleSubmit} className="submit-btn">
+            start
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default SetupForm;
