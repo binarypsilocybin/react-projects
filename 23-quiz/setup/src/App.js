90bb52911d02900c2d30c325fc446231133c640c
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-11-08 17:37:18 +0000

    display question

--- 23-quiz/setup/src/App.js
+++ 23-quiz/setup/src/App.js
@@ -13,7 +13,35 @@ function App() {
   if (loading) {
     return <Loading />;
   }
-  return <main>quizz app</main>;
+  console.log(questions);
+  const { question, incorrect_answers, correct_answer } = questions[5];
+
+  const answers = [...incorrect_answers, correct_answer];
+  return (
+    <main>
+      <Modal />
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers : {correct} / {index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                ></button>
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question">next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
