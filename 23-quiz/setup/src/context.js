5b41d20b58543841152ae78fb150ada265fe398a
binarypsilocybin <rogeriosvaldo@gmail.com>
2022-11-08 12:44:23 +0000

    fetch questions

--- 23-quiz/setup/src/context.js
+++ 23-quiz/setup/src/context.js
@@ -16,7 +16,7 @@ const tempUrl =
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [waiting, setWaiting] = useState(false);
+  const [waiting, setWaiting] = useState(true);
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [index, setIndex] = useState(0);
@@ -24,6 +24,30 @@ const AppProvider = ({ children }) => {
   const [error, setError] = useState(false);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch((err) => console.log(err));
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
 
   return (
     <AppContext.Provider
